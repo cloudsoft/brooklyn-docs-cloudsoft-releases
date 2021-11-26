248b2b2d232164afef6b5dd987a314d0e6988289
Mateusz Zan <mateusz.zan@cloudsoftcorp.com>
2021-11-26 13:49:26 +0000

    Release version 1.1.0-20211126-0922

--- _plugins/brooklyn_metadata.rb
+++ _plugins/brooklyn_metadata.rb
@@ -7,7 +7,7 @@
 #
 module BrooklynMetadata
 
-  BROOKLYN_VERSION = "1.1.0-SNAPSHOT" unless defined? BROOKLYN_VERSION
+  BROOKLYN_VERSION = "1.1.0-20211126-0922" unless defined? BROOKLYN_VERSION
 
   class Generator < Jekyll::Generator
     def generate(site)
