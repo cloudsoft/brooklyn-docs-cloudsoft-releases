a11fec925b9a08877bbae5f57e972c8f78ed7345
Cloudsoft Releaser <aled@cloudsoftcorp.com>
2021-12-03 11:35:43 +0000

    Release version 1.1.0-20211202.1837

--- _plugins/brooklyn_metadata.rb
+++ _plugins/brooklyn_metadata.rb
@@ -7,7 +7,7 @@
 #
 module BrooklynMetadata
 
-  BROOKLYN_VERSION = "1.1.0-SNAPSHOT" unless defined? BROOKLYN_VERSION
+  BROOKLYN_VERSION = "1.1.0-20211202.1837" unless defined? BROOKLYN_VERSION
 
   class Generator < Jekyll::Generator
     def generate(site)
