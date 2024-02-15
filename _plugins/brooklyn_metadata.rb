6578fb8c50a4cdec3e1104ca380475a6bcadb5ab
Cloudsoft Docker Build <info@cloudsoft.io>
2024-02-15 00:03:34 +0000

    Release version 1.2.0-20240214-2214

--- _plugins/brooklyn_metadata.rb
+++ _plugins/brooklyn_metadata.rb
@@ -7,7 +7,7 @@
 #
 module BrooklynMetadata
 
-  BROOKLYN_VERSION = "1.2.0-SNAPSHOT" unless defined? BROOKLYN_VERSION
+  BROOKLYN_VERSION = "1.2.0-20240214-2214" unless defined? BROOKLYN_VERSION
 
   class Generator < Jekyll::Generator
     def generate(site)
